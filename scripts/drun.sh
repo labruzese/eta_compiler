326c36aad658f345915cbdad60a1b72ac5890aad
Skylar Abruzese <skylar@abruzese.com>
2025-12-21 15:28:03 -0500

    update drun to use . as the shared directory

--- scripts/drun.sh
+++ scripts/drun.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-SHARED_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")/../shared" && pwd)
+SHARED_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 docker run -it -v "$SHARED_PATH":/home/student/shared charlessherk/cs4120-vm
