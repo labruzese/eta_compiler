15997737aceeb0b43dfeb412d24ea45b743afa73
Skylar Abruzese <skylar@abruzese.com>
2026-05-29 15:47:56 -0400

    driver: fixup with new lex and parse api + apply formatter

--- compiler/etac_driver/src/lib.rs
+++ compiler/etac_driver/src/lib.rs
@@ -1,7 +1,8 @@
+use etac_errors::{emit, error, Diagnostic, Level};
 use etac_lexer::Token;
+use etac_parse::ParseResult;
 use etac_session::{cli::Flags, logger::Logger};
 use etac_span::{FileId, InterfaceId, SourceId, Sources};
-use etac_errors::{emit, error, Diagnostic};
 
 pub fn run(flags: Flags) -> Result<(), ()> {
     let mut cache = Sources::new();
@@ -25,12 +26,7 @@ pub fn run(flags: Flags) -> Result<(), ()> {
     Ok(())
 }
 
-fn drive<Out, Parser>(
-    flags: &Flags,
-    cache: &mut Sources,
-    logger: &Logger,
-    files: &[FileId],
-) -> Result<(),()> 
+fn drive<Out, Parser>(flags: &Flags, cache: &mut Sources, logger: &Logger, files: &[FileId]) -> Result<(), ()>
 where
     Parser: etac_parse::IParser<Out>,
     Out: std::fmt::Display,
@@ -43,61 +39,112 @@ where
                 return Err(());
             }
         };
-        
+
         let mut lex_logging = flags.lex;
-        let mut parse_logging = flags.parse;
+        let parse_logging = flags.parse;
 
         let tok_map_fn = |lex_result| {
             if lex_logging {
                 token_callback(logger, file_id, &cache, &lex_result)?;
-                if lex_result.is_err() { lex_logging = false }
+                if lex_result.is_err() || matches!(lex_result, Ok((_, Token::Error(_), _))) {
+                    lex_logging = false
+                }
             }
             lex_result
         };
-        let mut parse_cb_fn = |parse_result: Result<Out, Diagnostic>| {
-            if parse_logging {
-                parse_callback(logger, file_id, &cache, &parse_result)?;
-                if parse_result.is_err() { parse_logging = false }
-            }
-            parse_result
-        };
 
-        let mut lexer = etac_lexer::Lexer::new(file_id.clone(), &source).map(tok_map_fn);
-        let parse_res = etac_parse::parse::<_, _, Parser, _>(file_id, &mut lexer, &mut parse_cb_fn);
+        let mut lexer = etac_lexer::Lexer::new(&cache, file_id.clone(), &source).map(tok_map_fn);
+        let parse_res = etac_parse::parse::<_, _, Parser, _>(file_id, &mut lexer, &mut |x| x);
 
-        if let Err(diags) = parse_res {
-            // drain lexer when parse crashes
-            lexer.for_each(drop);
-            for d in diags {
-                emit(cache, d);
+        match parse_res {
+            ParseResult {
+                output: Some(out),
+                errors: empty,
+            } if empty.is_empty() => {
+                if parse_logging {
+                    logger.log_parse(&file_id, out).map_err(|e| emit(cache, e.into()))?;
+                };
+                eprintln!("successfully parsed :)");
             }
-            return Err(());
+            ParseResult {
+                output: Some(_out),
+                errors: diags,
+            } if diags.iter().any(|d| d.level == Level::Error) => {
+                if parse_logging {
+                    let diag = diags.iter().find(|d| d.level == Level::Error).unwrap();
+                    let loc = cache
+                        .lc_index(
+                            file_id,
+                            diag.loc
+                                .as_ref()
+                                .expect("syntactic error must have location")
+                                .range
+                                .start,
+                        )
+                        .map_err(|e| emit(cache, e.into()))?;
+                    logger
+                        .log_syntactic_error(&file_id, loc, &diag.message)
+                        .map_err(|e| emit(cache, e.into()))?;
+                }
+                for d in diags {
+                    emit(cache, d);
+                }
+                eprintln!("parse tree can still be typechecked");
+            }
+            ParseResult {
+                output: None,
+                errors: diags,
+            } => {
+                debug_assert!(!diags.is_empty());
+                // drain lexer for more diagnostics
+                lexer.for_each(drop);
+                if parse_logging {
+                    let diag = diags.iter().find(|d| d.level == Level::Error).unwrap();
+                    let loc = cache
+                        .lc_index(
+                            file_id,
+                            diag.loc
+                                .as_ref()
+                                .expect("syntactic error must have location")
+                                .range
+                                .start,
+                        )
+                        .map_err(|e| emit(cache, e.into()))?;
+                    logger
+                        .log_syntactic_error(&file_id, loc, &diag.message)
+                        .map_err(|e| emit(cache, e.into()))?;
+                }
+                for d in diags {
+                    emit(cache, d)
+                }
+                eprintln!("couldn't build parse tree");
+            }
+            _ => unreachable!(),
         }
     }
     Ok(())
 }
 
-fn token_callback(logger: &Logger, file_id: &FileId, cache: &Sources, lex_result: &Result<(usize, Token, usize), Diagnostic>) -> Result<(), Diagnostic> {
+fn token_callback(
+    logger: &Logger,
+    file_id: &FileId,
+    cache: &Sources,
+    lex_result: &Result<(usize, Token, usize), Diagnostic>,
+) -> Result<(), Diagnostic> {
     match lex_result {
         Ok((start, tok, _end)) => {
             let loc = cache.lc_index(file_id, *start)?;
             logger.log_token(file_id, loc, tok)?;
-        },
+        }
         Err(diag) => {
-            let loc = cache.lc_index(file_id, diag.loc.as_ref().expect("lexcial error must have location").range.start)?;
-            if diag.level == etac_errors::Level::Error { logger.log_lexical_error(file_id, loc, &diag.message)? };
-        },
-    }
-    Ok(())
-}
-
-fn parse_callback<O: std::fmt::Display>(logger: &Logger, file_id: &FileId, cache: &Sources, parse_result: &Result<O, Diagnostic>) -> Result<(), Diagnostic> {
-    match parse_result {
-        Ok(out) => logger.log_parse(file_id, out)?,
-        Err(diag) => {
-            let loc = cache.lc_index(file_id, diag.loc.as_ref().expect("syntactic error must have location").range.start)?;
-            if diag.level == etac_errors::Level::Error { logger.log_syntactic_error(file_id, loc, &diag.message)? };
-        },
+            let loc = cache.lc_index(
+                file_id,
+                diag.loc.as_ref().expect("lexcial error must have location").range.start,
+            )?;
+            if diag.level == etac_errors::Level::Error {
+                logger.log_lexical_error(file_id, loc, &diag.message)?
+            };
+        }
     }
     Ok(())
 }
